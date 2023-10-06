910df996cd0aca000566b6b30822b04dfdc127e2
CloudRiRi15 <rejoiceymucheri@gmail.com>
2023-10-06 07:23:18 +0000

    #20 change structure of root module and add tagging to bucket

--- main.tf
+++ main.tf
@@ -9,11 +9,10 @@ resource "random_string" "bucket_name" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
 
- # tags = {
- #   UserUuid = var.user_uuid
- # }
-}  
- 
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-#variable "user_uuid" {
-#  description = "The UUID of the user."
-#  type        = string
-#  validation {
-#    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.user_uuid))
-#    error_message = "Invalid UUID format. It should be in the form of a UUID (e.g., 123e4567-e89b-12d3-a456-426614174000)."
-#  }
-#}
+variable "user_uuid" {
+  description = "The UUID of the user"
+  type        = string
+  validation {
+    condition        = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
+    error_message    = "The user_uuid value is not a valid UUID."
+  }
+}
