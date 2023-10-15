f372939b194499cec2933644d6716836863db6c2
Rejoice Mucheri <106775978+CloudRiRi15@users.noreply.github.com>
2023-10-16 01:02:03 +0200

    43 terratowns custom provider (#47)
    * #43 wip setting up the skeleton for creating a custom terraform provider
    
    * #43 implement terraform provider block in terraform
    
    * #43 setup skeleton for resource
    
    * wip still building custom provider
    
    * #43 finished implementing terraform custom provider

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 } 
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -29,3 +29,18 @@ provider "terratowns" {
 #  error_html_filepath = var.error_html_filepath
 #  content_version = var.content_version
 #}
+
+resource "terratowns_home" "home" {
+  name = "Forza MotorSport- Car Racing at it's best!!"
+  description = <<DESCRIPTION
+Forza Motorsport was released on May 3, 2005, and is the first installment in the 
+Forza Motorsport series. It is the only title in the series to be released on the 
+original Xbox console.It features 231 cars and racetracks from 15 real-world and 
+fictional locations. Here you get to witness the best of Forza Motorsport
+with some of the fastest cars in action.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
